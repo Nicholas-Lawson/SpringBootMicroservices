3dfc38091f199e53f056aa42868cd3e0675a5529
Nicholas-Lawson <lawsonnj@mail.uc.edu>
2021-09-07 15:15:24 -0400

    Update PlantplacesApplication.java

--- src/main/java/com/plantplaces/PlantplacesApplication.java
+++ src/main/java/com/plantplaces/PlantplacesApplication.java
@@ -3,6 +3,8 @@ package com.plantplaces;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/* Test comment */
 @SpringBootApplication
 public class PlantplacesApplication {
 
