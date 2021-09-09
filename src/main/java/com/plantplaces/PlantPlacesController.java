f11671c41b94ff1032064d8e11c9b9c161e3dd63
Nicholas-Lawson <lawsonnj@mail.uc.edu>
2021-09-09 14:40:34 -0400

    Update / endpoints

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -4,8 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+
+/**
+ * Handle the /start endpoint
+ * @return
+ *
+ */
+
 @Controller
 public class PlantPlacesController {
+	
+	
+	/**
+	 * Handle the / endpoint 
+	 * @return
+	 */
 	
 	@RequestMapping("/start")
 	public String start() {
